58c6a33bc41fa9a0f1b1f1f61f33dd7ba91635bd
Alex Nduta <wasremoved@gmail.com>
2023-08-10 19:58:58 +0300

    task 1 debugging

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ a = 10
 b = 5
 
 print("{:d} + {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d}".format(a, b, add(a, b)))
-print("{:d} * {:d}".format(a, b, add(a, b)))
-print("{:d} / {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d}".format(a, b, div(a, b)))
