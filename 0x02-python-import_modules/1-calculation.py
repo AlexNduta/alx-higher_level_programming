6e784d588f11c0bc2d7177e7a459601650130e78
Alex Nduta <wasremoved@gmail.com>
2023-08-10 19:23:53 +0300

    import statements and bliss

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+a = 10
+b = 5
+print("10 + 5", cal.add(a, b))
+print("10 - 5", cal.sub(a, b))
+print("10 * 5", cal.mul(a, b))
+print("10 / 5", cal.div(a, b))
