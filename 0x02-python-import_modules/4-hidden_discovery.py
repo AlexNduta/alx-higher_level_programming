8cb30d01b23b2ed9ae71e67bdeb1dd0ff87a2566
Alex Nduta <wasremoved@gmail.com>
2023-08-10 22:52:28 +0300

    decyphering

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    # import our provided file
     import hidden_4
-
+    # loop through the whole file to check for characters begining with "__"
     for string in dir(hidden_4):
         if string[:2] != "__":
-            print(f"{s}")
+            print(f"{:s}".format(s))
