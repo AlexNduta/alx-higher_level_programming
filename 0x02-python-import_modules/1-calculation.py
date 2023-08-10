37c48a0083d9a68cc0a9099e711f73daa95f5a89
Alex Nduta <wasremoved@gmail.com>
2023-08-10 19:58:22 +0300

    task 1 debugging

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
-    import calculator_1 as cal
+    import calculator_1
 
 a = 10
 b = 5
 
-print(f"{a} + {b}", cal.add(a, b))
-print(f"{a} - {b}", cal.sub(a, b))
-print("{a} * {b}", cal.mul(a, b))
-print("{a} / {b}", cal.div(a, b))
+print("{:d} + {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d}".format(a, b, add(a, b)))
+print("{:d} * {:d}".format(a, b, add(a, b)))
+print("{:d} / {:d}".format(a, b, add(a, b)))
