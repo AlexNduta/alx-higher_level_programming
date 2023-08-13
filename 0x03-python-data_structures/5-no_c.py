c9f871ca45fb923d4a96ed70312d634488cfb453
Alex Nduta <wasremoved@gmail.com>
2023-08-13 17:12:47 +0300

    task 5 debugging

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 
 def no_c(my_string):
     if my_string != []:
-        table = str.maketrans({"C":"", "c":""})
+        table = str.maketrans({"C": "", "c": ""})
         cleaned_string = my_string.translate(table)
         return cleaned_string
