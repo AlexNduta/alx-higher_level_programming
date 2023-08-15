708c4f55204f28a137001030bd9a57d4eee70e93
Alex Nduta <wasremoved@gmail.com>
2023-08-15 16:38:54 +0300

    task 3 debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
-if __name___ == "__main__"
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argumnet:")
-else:
-    print("{} arguments:".format(count))
-for x in range(count):
-    print("{}: {}".format(x + 1, sys.argv[x + 1]))
+if __name___ == "__main__":
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argumnet:")
+    else:
+        print("{} arguments:".format(count))
+    for x in range(count):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
