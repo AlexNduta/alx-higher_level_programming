9ae3a2176813f79ef8caf13a52b71454a6c12e8f
Alex Nduta <wasremoved@gmail.com>
2023-08-16 21:19:42 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    #define an empty list to hold each element
+    # define an empty list to hold each element
     squared_matrix = []
-    for row in matrix: #iterate through each row in the matrix
-        squared_row = [item ** 2 for item in row] # iterate through the each item in the row and square it
+    for row in matrix:  # iterate through each row in the matrix
+        # iterate through the each i    tem in the row and square it
+        squared_row = [item ** 2 for item in row]
         squared_matrix.append(squared_row)
     return squared_matrix
