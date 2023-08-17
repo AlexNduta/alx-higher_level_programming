bb9e8aa6a0caf0634f72d3236359ab789d242a91
Alex Nduta <wasremoved@gmail.com>
2023-08-17 05:56:03 +0300

    fixinf issues

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-def print_sorted_dictionary(a_dictionary):
-    key_elements = list(a_dictionary.keys())
-
-    for key in sorted(a_dictionary.keys()):
-        print("{}:{}".format(key, a_dictionary[key]))
+def print_sorted_dictionary(my_dict):
+    for key in sorted(my_dict.keys()):
+        print("{:s}: {}".format(key, my_dict[key]))
