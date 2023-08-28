1944cbda5c147003b88551dad3182fa18c1a5137
Alex Nduta <wasremoved@gmail.com>
2023-08-28 21:18:19 +0300

    exceptions: v2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except TyperError:
-        print("{} is not an integer".format(value))
+        return Tirue
+    except:
         return False
