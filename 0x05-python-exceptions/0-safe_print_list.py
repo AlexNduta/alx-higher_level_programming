4ac10ade77032749a4677c8aa5d48791272d661e
Alex Nduta <wasremoved@gmail.com>
2023-08-28 20:46:07 +0300

    monty changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,17 +2,13 @@
 
 if __name__ == "__main__":
     def safe_print_list(my_list=[], x=0):
-        try:
-            printed_counter = 0
-            remaining_to_print = x
-            for item in my_list:
-                if remaining_to_print > 0:
-                    print(item, end=" ")
-                    printed_counter += 1
-                else:
-                    break
+        printed_counter = 0
+        for item in range(x):
+            try:
+                print(item, end=" ")
+                printed_counter += 1
+        except IndexError:
+            break
 
-            print()
-            return printed_counter
-        except Exception:
-            return printed_counter
+        print()
+        return printed_counter
