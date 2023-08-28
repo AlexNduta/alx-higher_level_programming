739dc538397c1ba34a343c60ee93093d3275d084
Alex Nduta <wasremoved@gmail.com>
2023-08-28 21:43:40 +0300

    exceptions: v2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list[], x=0):
+    printed_item = 0
+    for item in range(x):
+        try:
+            print("{:d}".format(item), end="")
+            printed_item += 1
+        except Exception:
+            pass
+        except IndexError as y:
+            print("{}".format(y))
+        else:
+            print()
+            return printed_item
