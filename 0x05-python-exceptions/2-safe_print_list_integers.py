f0a6d2b697381963831baa1aa4340f974dddfcd6
Alex Nduta <wasremoved@gmail.com>
2023-08-28 22:05:55 +0300

    exceptions: v2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,10 @@ def safe_print_list_integers(my_list[], x=0):
     printed_item = 0
     try:
         for item in my_list[:x]:
-            if isinstance(item, int )
+            if isinstance(item, int)
             print("{:d}".format(item), end="")
             printed_item += 1
     except Exception as e:
-            print(e)
+        print(e)
     print()
     return printed_item
