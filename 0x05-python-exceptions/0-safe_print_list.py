1355dc298dbe24e1e006b6989f44b160218d1915
Alex Nduta <wasremoved@gmail.com>
2023-08-28 20:54:36 +0300

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[item], end="")
             printed_counter += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
 
     print()
     return printed_counter
