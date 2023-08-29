abb91c873cf99c56f1484b240e7630cc7997a0b1
Alex Nduta <wasremoved@gmail.com>
2023-08-29 04:18:45 +0300

    exceptions: version 3

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,10 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         except ZeroDivisionError:
             print("division by zero")
+            result = 0
         except TypeError:
             print("wrong type")
+            result = 0
         finally:
-            return result
+            print("{}".format(result))
+    return result
