a3a3199071f1f667c6a0825066e212b131c542d3
Alex Nduta <wasremoved@gmail.com>
2023-08-29 03:30:46 +0300

    exceptions: version 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,10 @@
 
 def safe_print_division(a, b):
     try:
-        reult = a/b
+        result = a/b
     except (ZeroDivisionError, ValueError) as ep:
         print(ep)
     else:
         return None
     finally:
-        return result
+        return print("{:d}".format(result))
