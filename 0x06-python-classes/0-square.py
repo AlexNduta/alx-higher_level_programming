fdd0088bfe9acf0f6c8828782fe1000e209860bd
Alex Nduta <wasremoved@gmail.com>
2023-08-30 05:29:09 +0300

    classes and objects:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A class called square that defines a square"""
+
+class Square:
+    """ Represents a Square object, contains all attributes of a square
+        
+        Attributes:
+                lenghth: This is the length of our Square
+    """
+    length(int)
