6a2f48bbb1ad7e305e65b33699d121a25c3a93ab
Alex Nduta <wasremoved@gmail.com>
2023-08-30 05:46:07 +0300

    classes and objects:

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+""" Defines a Sauare object"""
+
+
+class Square:
+    """ This class defines the property of the Square object
+        Attribute:
+            size(int): this is the size of our square
+    """
+    size
