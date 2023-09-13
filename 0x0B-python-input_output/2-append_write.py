c80da8f9e173faa1e64b37fea1e1a778fbf167a8
Alex Nduta <wasremoved@gmail.com>
2023-09-13 04:41:55 +0300

    file IO in python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,5 +8,5 @@ fnction that appends a string
 
 def append_write(filename="", text=""):
     """returns the number of characters added:"""
-    with open(filename, 'a', encoding="utf-8") as file:
-        return(f.write(text))
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
