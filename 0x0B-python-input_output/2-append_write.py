579be0388649e4b0fa301cecf3369d4455a6f31b
Alex Nduta <wasremoved@gmail.com>
2023-09-13 04:35:56 +0300

    file IO in python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" this is the funcition 'append_write'
-    this function intends on writting to our file and incase
-    the filen does not exist it is created afresh
+"""
+Module 2-read_lines
+
+fnction that appends a string
 """
 def append_write(filename="", text=""):
-    """opening the file enables us to make any operations on out file.
-        in this case, we will be appening to our file if it exist
-    """
+    """returns the number of characters added:"""
     with open(filename, 'a') as  file:
-        appending_to_file = file.write(text)
-        return appending_to_file
+        return f.write(text)
