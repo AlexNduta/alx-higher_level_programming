b1007f32c82d261f786be5b019330a882623c0ea
Alex Nduta <wasremoved@gmail.com>
2023-09-13 04:26:22 +0300

    file IO in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ A function that reads text file and prints it to stdout
 
 def read_file(filename=""):
     """ our function takes in the name of the file as a parameter"""
-    with open(filename,'r', encoding="utf-8") as bs:
+    with open(filename, mode="r", encoding="utf-8") as bs:
         print(bs.read(), end="")
