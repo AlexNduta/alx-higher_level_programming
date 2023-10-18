222cae86ba954ca82e39879d0c05ce3b87c25834
was <was@kali.was.me>
2023-10-18 04:54:37 +0300

    SQL databases

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- list al recors of our database
+
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- create 'second_table' if it does not exist
+-- have 3 rows; id, name & score
+-- put in records
+
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
+(1, 'john', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
