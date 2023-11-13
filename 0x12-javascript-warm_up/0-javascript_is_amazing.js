81282be5e0fbc84f57b4adc14691f98e6a90e850
Alex Nduta <wasremoved@gmail.com>
2023-11-13 17:06:44 +0300

    edited readme

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-/* prints a single statement to console */
+
 const myVar = 'JavasScript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* prints multiple statements */
+
 const c = 'C is fun';
 const py = 'Python is cool';
 const js = 'Javascript is amazing';
