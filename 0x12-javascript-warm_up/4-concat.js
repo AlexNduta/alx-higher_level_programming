b4a4548590582f9f2d59043cec91c775cf90ac4f
Alex Nduta <wasremoved@gmail.com>
2023-11-14 04:23:53 +0300

    JS warmup task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const i = 2;
-const argv1 = process.argv[2];
-const argv2 = process.argv[3];
-
+let argv1 = process.argv[2];
+let argv2 = process.argv[3]
 console.log(argv1 + ' is ' + argv2);
