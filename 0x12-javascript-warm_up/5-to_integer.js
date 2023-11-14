7b479fa8e7ddc0b7004e73c98613ef23f2816dfd
Alex Nduta <wasremoved@gmail.com>
2023-11-14 15:21:10 +0300

    JS warmup task 5 -  fixing errors

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const argv = parseInt(process.argv[2], 10); // convert the argument to an integer
-//const text = 'My number: ';
 // check if the argument is a number
 if (isNaN(argv)) {
   console.log('Not a number');
