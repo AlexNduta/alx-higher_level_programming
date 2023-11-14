e1d0ae66ed8aafe10c0740dc2fbe34a65a408c56
Alex Nduta <wasremoved@gmail.com>
2023-11-14 14:10:14 +0300

    JS warmup task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-const argv = Math.floor(Number(process.argv[2])); // convert the argument to an integer
+const argv = parseInt(process.argv[2], 10); // convert the argument to an integer
 const text = 'My number: ';
+// check if the argument is a number
 if (isNaN(argv)) {
   console.log('Not a number');
 } else if (argv !== undefined) {
