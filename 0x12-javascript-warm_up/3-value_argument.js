d9bde7d0e24ffd3a5b06b6aedf999f839d252c41
Alex Nduta <wasremoved@gmail.com>
2023-11-14 03:46:28 +0300

    JS warmup task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const varg = process.argv;
 
-for (let i = 2; i <= varg.length - 1; i++)
-{
-    if (varg[i] !== null)
-    {
-        console.log(varg[i]);
-    } else {
-        console.log('No argument');
-    }
+for (let i = 2; i <= varg.length - 1; i++) {
+  if (varg[i] !== null) {
+    console.log(varg[i]);
+  } else {
+    console.log('No argument');
+  }
 }
