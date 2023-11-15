1102767c31dc86653bd7ba4645e9142cc0de1bc1
Alex Nduta <wasremoved@gmail.com>
2023-11-15 19:38:13 +0300

    classes and objects

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
 const rec = require('./4-rectangle.js');
 
 module.exports = class Square extends rec {
-    constructor (size) {
-        super(size, size);
-
-    }
+  constructor (size) {
+    super(size, size);
+  }
 };
