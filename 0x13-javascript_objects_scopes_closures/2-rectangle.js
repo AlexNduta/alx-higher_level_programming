9567ce6674420d206baec328df803a62f1d5cac1
Alex Nduta <wasremoved@gmail.com>
2023-11-15 12:33:43 +0300

    JS classes task 2 - fixing errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,14 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0){
-        module.exports = class Rectangle {}
+    if (w <= 0 || h <= 0) {
+      module.exports = class Rectangle {};
     }
 
     this.width = w;
     this.height = h;
   }
-}
+};
 
 // export as a module
 /*
