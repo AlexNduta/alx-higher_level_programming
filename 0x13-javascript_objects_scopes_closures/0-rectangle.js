a7e715d87852a253fbe3214d4411345715bbab31
Alex Nduta <wasremoved@gmail.com>
2023-11-15 10:11:15 +0300

    JS classes task 0- fixing errors

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
+/*
 class Rectangle {
   constructor (width, length) {
     this.width;
     this.length;
   }
-}
+} */
 
 // Exort the module for use by other methods
-module.exports = Rectangle;
+module.exports = class Rectangle {};
