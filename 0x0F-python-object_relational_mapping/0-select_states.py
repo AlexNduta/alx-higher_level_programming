32e99786651527971689c50717d0fa35558b8a45
Alex Nduta <wasremoved@gmail.com>
2023-11-18 14:55:04 +0300

    MYSQL in python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+db = MySQLdb.connect(
+        host="localhost",
+        user="root",
+        passwd="BinaryKid",
+        db="hbtn_0e_0_usa"
+        )
+cur = db.cursor()
+cur.execute("SELECT states FROM hbtn_0e_0_usa")
