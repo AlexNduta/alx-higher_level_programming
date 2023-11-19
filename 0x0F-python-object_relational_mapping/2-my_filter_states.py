46f406e4aaedb1f6e70c523b130445f917a1a6fc
Alex Nduta <wasremoved@gmail.com>
2023-11-19 15:23:05 +0300

    Msql in python task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,23 +7,28 @@ Takes an argument and displays all values in the `states`ntable of `hbtn_0e_0_us
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    # creaate a connector
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            name= input(argv[4])
-            )
-    # create a cursor
-    cur = db. cursor()
+    try:
+        # creaate a connector
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3],
+                name= input(argv[4])
+                )
+        # create a cursor
+        cur = db. cursor()
 
-    # start executing statements
-    cur.execute("SELECT * FROM state WHERE name='{name}' ORDER BY id ASC;")
-    for row  in cur.fetchall():
-        print(row)
-    cur.close()
-    db.close()
+        # start executing statements
+        cur.execute("SELECT * FROM state WHERE name='%s' ORDER BY id ASC;", (name))
+        for row  in cur.fetchall():
+            print(row)
+    except:
+        print("Error")
+
+    finally:
+        cur.close()
+        db.close()
 
 
