718c3493fe0e33fc71d3f6a1b3e8d9c3dd544b8b
Alex Nduta <wasremoved@gmail.com>
2023-11-19 20:33:55 +0300

    Msql in python task 2 - fixing errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""
-Takes an argument and displays all values in the `states`ntable of `hbtn_0e_0_usa` where `name` matches the argument
-"""
 
-#import necesary modules
 import MySQLdb
 from sys import argv
-if __name__ == "__main__":
-    try:
-        # creaate a connector
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=argv[1],
-                passwd=argv[2],
-                db=argv[3]
-               # name= input(argv[4])
-                )
-        # create a cursor
-        cur = db. cursor()
 
-        # start executing statements
-        cur.execute("SELECT * FROM state WHERE name LIKE '{:s}' ORDER BY id ASC;".format(argv[4]))
-        for row  in cur.fetchall():
+if __name__ == "__main__":
+
+    try:
+        # Connect to the database
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+
+        # Create a cursor to execute queries using SQL;
+        cursor = db.cursor()
+        sql_cmd = """SELECT *
+                        FROM states
+                        WHERE name LIKE '{:s}'
+                        ORDER BY id ASC""".format(argv[4])
+        cursor.execute(sql_cmd)
+
+        # Fetch and process the results
+        for row in cursor.fetchall():
             if row[1] == argv[4]:
                 print(row)
-    except:
-        print("Error")
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
 
     finally:
-        cur.close()
+        cursor.close()
         db.close()
-
-
