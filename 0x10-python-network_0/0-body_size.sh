ff929957c877713038feb9e63ec7fe409baee5ea
Alex Nduta <wasremoved@gmail.com>
2023-12-01 16:05:44 +0300

    python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
-#calculates the total bytes of a http header give. how to use /0-boody_size.sh 
-curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r\n'
-
+#!/bin/bash
+#calvulates the total bytes of a http header give. how to use /0-boody_size.sh 
+curl -s "$1" | wc -c
