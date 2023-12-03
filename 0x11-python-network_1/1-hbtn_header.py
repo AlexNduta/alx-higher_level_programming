9a8674d67ecd7c7e2bbeb5c5300b618b345db72a
Alex Nduta <wasremoved@gmail.com>
2023-12-03 15:18:13 +0300

    python networking: get requests

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-takes a URL and displays the value of the X-Requrst-Id
-found in the header of the response
+takes a URL and displays the value of the X-Requrst-Id found in the header of the response
 """
 from urllib.request import urlopen
 import sys
 url = sys.argv[1]
 with urlopen(url) as response:
-    header = response.headers
-    print(header['X-Request-Id'])
+    print(response.getheader('X-Request-Id'))
