adf64340c8dca345bb8262c84a2b959ae7424997
Alex Nduta <wasremoved@gmail.com>
2023-12-03 14:30:56 +0300

    python networking: get requests

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,4 +5,7 @@ from urllib.request import urlopen
 url = "https://alx-intranet.hbtn.io/status"
 with urlopen(url) as response:
     body = response.read()
-print(body)
+print('Body response:')
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf8')))
