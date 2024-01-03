7a321c0e25051f4d3f43559dac83c976fbca20c9
Alex Nduta <wasremoved@gmail.com>
2024-01-03 15:05:38 +0300

    web scrapping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//read and prints cntents of a file
+const fs = require('fs'); //import 'fs' mosule for reading files
+const input = process.argv[2];  //create the input variable
+
+//read the file using the `readFile` function
+fs.readFile(input, function(err,data){
+    console.log(data.toString());
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+# web scrapping with JS
+ tool: request v2.88.2
+ semistandard v17.0.0
