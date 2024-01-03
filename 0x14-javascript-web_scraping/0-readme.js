bf37802f0ec13984bacdd1aac6701b4b089eed46
Alex Nduta <wasremoved@gmail.com>
2024-01-03 15:24:20 +0300

    JS:web scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,6 +5,9 @@ const input = process.argv[2];  //create the input variable
 
 //read the file using the `readFile` function
 fs.readFile(input, 'utf8', function(err,data) {
+    if (err){
+        console.log(err);
+    } else {
     console.log(data.toString());
-    
+    }
 });
