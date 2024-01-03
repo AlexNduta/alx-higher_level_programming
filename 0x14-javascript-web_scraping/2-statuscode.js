190209a82df573e87f8a82e9f8be42fcbd20a3d8
Alex Nduta <wasremoved@gmail.com>
2024-01-03 20:40:44 +0300

    print status code of the GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, function(error, response, body){
-    console.error('error:'.error);
+   //console.error('error:'.error);
     console.log('code:', response && response.statusCode);
 });
