aad013e9bb65debc5863127c2fc13dea08e4966a
Alex Nduta <wasremoved@gmail.com>
2024-01-03 21:46:41 +0300

    print status code of the GET request

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,16 @@
-#!/usr/bin/node 
-//makes aget request to the starwars API
+#!/usr/bin/node
+// makes aget request to the starwars API
 
 const request = require('request');
 const ID = process.argv[2];
-const url = "https://swapi-api.alx-tools.com/api/films/:id"
-request(url, function(error, response, body)){
-    console.log(body);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + ID;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body.title);
+  } else {
+    console.log('ERRO!', response.statusCode);
+  }
 });
