8a2c908d6f51a13ea94a32fcc4d83c06b84fbdcb
Alex Nduta <wasremoved@gmail.com>
2024-01-03 15:35:12 +0300

    JS:web scrapping; reading from a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-# 
+// writes contents to a file
+const fs = require('fs');
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+fs.writeFile(arg1, arg2, (err) => {
+    if (err){
+        console.log(err);
+    }
+});
